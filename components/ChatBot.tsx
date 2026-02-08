2c5738b88f4ab790e1db37d3949f026739fea116
Anant Kumar Singh <anantk.singh9876@gmail.com>
2026-02-08 15:36:52 -0500

    Added final touches

--- components/ChatBot.tsx
+++ components/ChatBot.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState, useRef, useEffect } from "react";
-import "./ChatBot.css";
+import styles from "./ChatBot.module.css";
 
 interface Message {
     role: "user" | "assistant";
